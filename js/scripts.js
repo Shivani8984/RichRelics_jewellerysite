b03d0210b2d48623f6086d9ca71658b6dbe4c45f
Shivani8984 <shivani8984@gmail.com>
2020-09-23 16:40:56 -0400

    added images to carousel

--- js/scripts.js
+++ js/scripts.js
@@ -25,20 +25,8 @@ $(window).on("load resize", function() {
     $dropdown.off("mouseenter mouseleave");
   }
 });
-$('.carousel').carousel();
 
-   /* $(".carousel").carousel( { interval: 2000 } );
-    $("#carouselButton").click(function(){
-        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-            $(".carousel").carousel("pause");
-            $("#carouselButton").children("i").removeClass("fa-pause");
-            $("#carouselButton").children("i").addClass("fa-play");
-        } else {
-            $(".carousel").carousel("cycle");
-            $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause"); 
-        }
-    });*/
+    $('.carousel').carousel();
 
     $("#reserveButton").click(function(){
         $('#reserveModal').modal('show');
